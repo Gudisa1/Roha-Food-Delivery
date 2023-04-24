fcef7c2315e0479e482b6b94d2ec76ed6e035a0f
Gudisa Gebi <gudisagebi1@gmail.com>
2023-04-23 23:51:18 -0700

    Image Upload Compelete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 import "./app.scss";
 
@@ -30,6 +31,10 @@ function App() {
         {
           path: "/login",
           element: <Login />,
+        },
+        {
+          path: "/register",
+          element: <Register />,
         },
       ],
     },
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,7 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Register.scss";
-const Register = () => {
-  return <div>Register</div>;
-};
+import newRequest from "../../utils/newRequest.js";
+import { useNavigate } from "react-router-dom";
+import upload from "../../utils/upload.js";
+
+function Register() {
+  const [file, setFile] = useState(null);
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    img: "",
+    phone: "",
+    isRestaurant: false,
+    isDelivery: false,
+    desc: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setUser((prev) => {
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  };
+
+  const handleRestaurant = (e) => {
+    setUser((prev) => {
+      return { ...prev, isRestaurant: e.target.checked };
+    });
+  };
+  const handleDelivery = (e) => {
+    setUser((prev) => {
+      return { ...prev, isDelivery: e.target.checked };
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const url = await upload(file);
+    try {
+      await newRequest.post("/auth/register", {
+        ...user,
+        img: url,
+      });
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="register">
+      <form onSubmit={handleSubmit}>
+        <div className="left">
+          <h1>Create a new account</h1>
+          <label htmlFor="">Username</label>
+          <input
+            name="username"
+            type="text"
+            placeholder="johndoe"
+            onChange={handleChange}
+          />
+          <label htmlFor="">Email</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="email"
+            onChange={handleChange}
+          />
+          <label htmlFor="">Password</label>
+          <input name="password" type="password" onChange={handleChange} />
+          <label htmlFor="">Profile Picture</label>
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
+          <button type="submit">Register</button>
+        </div>
+        <div className="right">
+          <h1>I want to become a Member</h1>
+          <div className="toggle">
+            <label htmlFor="">Activate the restaurant account</label>
+            <label className="switch">
+              <input type="checkbox" onChange={handleRestaurant} />
+              <span className="slider round"></span>
+            </label>
+          </div>
+          <div className="toggle">
+            <label htmlFor="">Activate the delivery account</label>
+            <label className="switch">
+              <input type="checkbox" onChange={handleDelivery} />
+              <span className="slider round"></span>
+            </label>
+          </div>
+          <label htmlFor="">Phone Number</label>
+          <input
+            name="phone"
+            type="text"
+            placeholder="+1 234 567 89"
+            onChange={handleChange}
+          />
+          <label htmlFor="">Description</label>
+          <textarea
+            placeholder="A short description of yourself"
+            name="desc"
+            id=""
+            cols="30"
+            rows="10"
+            onChange={handleChange}
+          ></textarea>
+        </div>
+      </form>
+    </div>
+  );
+}
 
 export default Register;
